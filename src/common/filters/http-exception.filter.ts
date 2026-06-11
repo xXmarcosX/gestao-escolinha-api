1709a19d1c26ad846ae19f634ece31e5f2ae5991
xXmarcosX <marcos.v3567@gmail.com>
2026-06-10 21:14:20 -0300

    add: HttpException Filter

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,21 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Response } from 'express';
+import { ExceptionResponse } from 'src/models/ExceptionResponse';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const exceptionResponse: any = exception.getResponse();
+
+    const messages = Array.isArray(exceptionResponse.message)
+      ? exceptionResponse.message
+      : [exceptionResponse.message || exception.message];
+
+    const errorPayload = new ExceptionResponse(messages, status);
+
+    response.status(status).json(errorPayload);
+  }
+}
