d0f57e2a14a442a841e9410626a71175765607a0
xXmarcosX <marcos.v3567@gmail.com>
2026-06-12 11:50:25 -0300

    fix: Add isActive field on CreateLocalTreinoDTO

--- src/local-treino/dto/create-local-treino.dto.ts
+++ src/local-treino/dto/create-local-treino.dto.ts
@@ -1,7 +1,10 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateLocalTreinoDto{
     @IsString({message:'O campo local nome deve estar preenchido'})
     @IsNotEmpty({message:'O campo local nome deve estar preenchido'})
     nomeLocal?: string;
+
+    @IsBoolean({message: "O campo 'isActive' deve ser um booleano"})
+    isActive?: boolean
 }
