70d0ceb7fba646f8d54c1ab22e95be6f07b70afe
ViniciusSegantini <vinicius_vdss@hotmail.com>
2026-07-21 16:28:50 -0300

    add: diaSemana Enum

---
+++ src/enums/diasSemana.ts
@@ -0,0 +1,9 @@
+export enum diasSemana {
+  DOMINGO = 'Domingo',
+  SEGUNDA = 'Segunda-feira',
+  TERCA = 'Terça-feira',
+  QUARTA = 'Quarta-feira',
+  QUINTA = 'Quinta-feira',
+  SEXTA = 'Sexta-feira',
+  SABADO = 'Sábado'
+}
