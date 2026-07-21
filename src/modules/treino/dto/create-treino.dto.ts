8c22a80bb4a8d4bb5ece44a9be4f0956f4967b50
ViniciusSegantini <vinicius_vdss@hotmail.com>
2026-07-21 16:33:44 -0300

    add: properties to Treino

--- src/modules/treino/dto/create-treino.dto.ts
+++ src/modules/treino/dto/create-treino.dto.ts
@@ -1,5 +1,6 @@
 import { Type } from "class-transformer";
-import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { diasSemana } from "src/enums/diasSemana";
 
 export class CreateTreinoDto {
   @IsNotEmpty({ message: 'O campo dataTreino não pode ser vazio.' })
@@ -10,6 +11,20 @@ export class CreateTreinoDto {
   @IsNotEmpty({ message: 'O campo descricao não pode ser vazio.' })
   @IsString({ message: 'O campo descricao deve ser uma string.' })
   descricao: string;
+
+  @IsNotEmpty({ message: 'O campo diaTreino não pode ser vazio.' })
+  @IsEnum(diasSemana, { message: 'O campo diaTreino deve ser um valor válido de dias da semana.' })
+  diaTreino: diasSemana;
+
+  @IsNotEmpty({ message: 'O campo horarioInicioTreino não pode ser vazio.' })
+  @Type(() => Date)
+  @IsDate({ message: 'O campo horarioInicioTreino deve ser uma data válida.' })
+  horarioInicioTreino: Date;
+
+  @IsNotEmpty({ message: 'O campo horarioFimTreino não pode ser vazio.' })
+  @Type(() => Date)
+  @IsDate({ message: 'O campo horarioFimTreino deve ser uma data válida.' })
+  horarioFimTreino: Date;
 
   @IsNotEmpty({ message: 'O campo instrutorId não pode ser vazio.' })
   @IsNumber({}, { message: 'O campo instrutorId deve ser um número.' })
--- src/modules/treino/entities/treino.entity.ts
+++ src/modules/treino/entities/treino.entity.ts
@@ -2,6 +2,7 @@ import { Instrutor } from "src/modules/instrutor/entities/instrutor.entity";
 import { LocalTreino } from "src/modules/local-treino/entities/local-treino.entity";
 import { Turma } from "src/modules/turma/entities/turma.entity";
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { diasSemana } from "src/enums/diasSemana";
 
 @Entity()
 export class Treino {
@@ -13,6 +14,15 @@ export class Treino {
 
   @Column({name: 'tre_descricao'})
   descricao: string;
+
+  @Column({name: 'tre_dia_treino'})
+  diaTreino: diasSemana;
+
+  @Column({name: 'tre_horario_inicio_treino'})
+  horarioInicioTreino: Date;
+
+  @Column({name: 'tre_horario_fim_treino'})
+  horarioFimTreino: Date;
 
   @ManyToOne(() => Instrutor)
   @JoinColumn({name: 'instrutor_id'})
