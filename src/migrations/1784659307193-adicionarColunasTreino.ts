81a5d686c8f59a4ce58918a75dc5352707005fc3
ViniciusSegantini <vinicius_vdss@hotmail.com>
2026-07-21 16:29:53 -0300

    seed: added new columns at treino table

---
+++ src/migrations/1784659307193-adicionarColunasTreino.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AdicionarColunasTreino1784659307193 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE treino 
+            ADD COLUMN tre_dia_treino TEXT DEFAULT NULL,
+            ADD COLUMN tre_horario_inicio_treino DATETIME NOT NULL,
+            ADD COLUMN tre_horario_fim_treino DATETIME NOT NULL;
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE treino 
+            DROP COLUMN tre_dia_treino,
+            DROP COLUMN tre_horario_inicio_treino,
+            DROP COLUMN tre_horario_fim_treino;
+        `);
+    }
+
+}
